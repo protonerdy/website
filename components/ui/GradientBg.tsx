ca0ba7f63b714c0c9fc53880692878e96d98baa1
Kushagra Shrivastava <76205591+protonerdy@users.noreply.github.com>
2024-12-23 00:06:22 +0530

    Update GradientBg.tsx
    Fixed SSR

--- components/ui/GradientBg.tsx
+++ components/ui/GradientBg.tsx
@@ -40,14 +40,18 @@ export const BackgroundGradientAnimation = ({
     const [tgX, setTgX] = useState(0);
     const [tgY, setTgY] = useState(0);
     useEffect(() => {
-    document.body.style.setProperty(
-        "--gradient-background-start",
-        gradientBackgroundStart
-    );
-    document.body.style.setProperty(
-        "--gradient-background-end",
-        gradientBackgroundEnd
-    );
+        if (typeof window !== 'undefined') {
+            document.body.style.setProperty(
+                "--gradient-background-start",
+                gradientBackgroundStart
+            );
+            document.body.style.setProperty(
+                "--gradient-background-end",
+                gradientBackgroundEnd
+            );
+        }
+}, [gradientBackgroundStart, gradientBackgroundEnd]);
+
     document.body.style.setProperty("--first-color", firstColor);
     document.body.style.setProperty("--second-color", secondColor);
     document.body.style.setProperty("--third-color", thirdColor);
